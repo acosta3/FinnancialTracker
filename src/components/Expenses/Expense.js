0cd204f60882da74a41e4c9fd9d454a0b2f51493
Anthony <acosta3@mmcaster.ca>
2021-11-09 13:22:47 -0500

    yeahey

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -1,45 +1,36 @@
-import React, {useState} from 'react';
-import ExpenseItem from "./ExpenseItem";
-import Card from "../UI/Card";
-import './Expense.css'
-import ExpensesFilter from "./ExpenseFilter";
 
-function Expense(prop){
+import React, { useState } from 'react';
 
-  const[filteredYear, setFilteredYear]=useState('2020');
-  const filterChangeHandler=(selectedYear)=>{
-    console.log('Expense.js');
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpenseFilter';
+import './Expense.css';
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-  }
+  };
 
-    return( 
-      <div>
-    <Card className="expenses">
-    <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
-    <ExpenseItem
-    title={prop.expense[0].title}
-    amount={prop.expense[0].amount}
-    date={prop.expense[0].date}
-  ></ExpenseItem>
-  <ExpenseItem
-    title={prop.expense[1].title}
-    amount={prop.expense[1].amount}
-    date={prop.expense[1].date}
-  ></ExpenseItem>
-  <ExpenseItem
-    title={prop.expense[2].title}
-    amount={prop.expense[2].amount}
-    date={prop.expense[2].date}
-  ></ExpenseItem>
-  <ExpenseItem
-    title={prop.expense[3].title}
-    amount={prop.expense[3].amount}
-    date={prop.expense[3].date}
-  ></ExpenseItem>
-  </Card>
-  </div>
-  )
-  
-}
+  return (
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            key={expense.id} //when mapping must adda  key
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
+  );
+};
 
- export default Expense;
+export default Expenses;
